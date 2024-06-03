8cbf5fde4166da32f43ab8ad816ddf8eee6b401f
Jacob <jacobrashid404@gmail.com>
2024-06-03 16:24:18 -0700

    prop is now a single 'dog' instead of 'dogs' array

--- src/DogDetails.jsx
+++ src/DogDetails.jsx
@@ -2,26 +2,24 @@ import { useParams } from "react-router-dom";
 
 /** DogDetails Component
  *
- * Props: dogs
- * State: None
+ * Props:
+ * dog: object
+ * eg. { name: string, age: number, src: string, facts: ["string"...] }
  *
+ * State:
+ * None
+ *
+ *
+ * DogFinder -> DogDetails
  */
 
-function DogDetails({ dogs }) {
-  const { name } = useParams();
-  const dog = dogs.find(dog => dog.name === name);
-
-  if (!dog) return <p>No dog found: {name}</p>
-
-  console.log({ dogs });
-  console.log({ dog });
-  console.log(dog.name);
-  console.log(dog.facts);
+function DogDetails({ dog }) {
+  if (!dog) return <p>No dog found: {dog.name}</p>
 
   return (
     <div>
       <h1>Name: {dog.name}</h1>
-      <img src={`./public/${dog.src}.jpg`} alt={`Picture of ${dog.name}`} />
+      <img src={`/${dog.src}.jpg`} alt={`Picture of ${dog.name}`} />
       <h3>Age: {dog.age}</h3>
       <p>Facts: {dog.facts.join(" ")}</p>
     </div>
