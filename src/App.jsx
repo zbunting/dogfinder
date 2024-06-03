88502a61b005916fed7307df889331662fee57e1
Zach Bunting <zvrbunting@gmail.com>
2024-06-03 14:08:49 -0700

    Part1 WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
+/** Component for dog viewing app
+ *
+ * Props: none
+ *
+ * State:
+ * - dogs: array of objects
+ *   initial state is []
+ *   eg. [ { name: string, age: number, src: string, facts: ["string"...] }... ]
+ *
+ * App -> {BrowserRouter, Nav, Routes, Route, DogList, DogDetails}
+*/
 function App() {
+  const [dogs, setDogs] = useState([]);
+
+  function handleDogList(dogsFromApi) {
+    setDogs(dogsFromApi);
+  }
+
   return (
     <div className="App">
-      <h1>Good luck!</h1>
+      <BrowserRouter>
+        <Nav dogNames={dogs.map(dog => dog.name)} />
+        <Routes>
+          <Route path="/dogs" element={<DogList fetchDogs={handleDogList} />} />
+          <Route path="/dogs/:name" element={<DogDetails dogs={dogs} />} />
+          <Route path="*" element={<Navigate to="/dogs" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
